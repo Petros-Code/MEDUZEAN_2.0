ceb1e2b48f79357ff5b3bfb82173c9714f672375
Petros-Code <petros-code@proton.me>
2025-09-23 16:30:53 +0200

    Nouvelle Version : DEBUG ajout EAN manuel + display erreurs/succes

--- wp-data/wp-content/plugins/meduzean/meduzean.php
+++ wp-data/wp-content/plugins/meduzean/meduzean.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Meduzean EAN Manager
  * Description: Gestion des codes EAN (import, assignation, alertes cron).
- * Version: 1.0.4
+ * Version: 1.0.5
  * Author: Petros-Code
  * Text Domain: meduzean
  * Domain Path: /languages
