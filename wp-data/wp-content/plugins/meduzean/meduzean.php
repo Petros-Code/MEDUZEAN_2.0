e0661540e696fa81c3c947f26178b6ef521e6a21
Petros-Code <petros-code@proton.me>
2025-09-24 11:06:59 +0200

    nouvelle mise à jour stable avant modif des filtres EAN

--- wp-data/wp-content/plugins/meduzean/meduzean.php
+++ wp-data/wp-content/plugins/meduzean/meduzean.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Meduzean EAN Manager
  * Description: Gestion des codes EAN (import, assignation, alertes cron).
- * Version: 1.0.5
+ * Version: 1.1.0
  * Author: Petros-Code
  * Text Domain: meduzean
  * Domain Path: /languages
